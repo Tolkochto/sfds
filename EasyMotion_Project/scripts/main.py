7aca7515221fd56f476bed1277afa536d3976bd2
Tolkochto <142894607+Tolkochto@users.noreply.github.com>
2026-08-22 18:40:45 +0300

    Update main.py

--- EasyMotion_Project/scripts/main.py
+++ EasyMotion_Project/scripts/main.py
@@ -118,13 +118,13 @@ def evaluate_squat_dtw_swapped(ref_video, my_video, output_video, model, device,
     if all_cos_sim:
         final_cos = np.mean(all_cos_sim)
         final_dist = np.mean(all_w_dist)
+        print()
         print(f"Среднее Косинусное сходство (Cosine Similarity): {final_cos:.3f}")
         print(f"Среднее Взвешенное совпадение (Weighted Distance): {final_dist:.1f} px")
     else:
         print("\nПредупреждение: не удалось собрать кадры для расчета метрик.\n")
         
     print(f"Готово! Результат сохранен в {output_video}")
-
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="AI-Тренер: Сравнение техники приседаний.")
